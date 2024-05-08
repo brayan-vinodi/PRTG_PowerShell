2b349d763f44775da0cb33405ea05384e42d6e0d
brayan-vinodi <brayan8vinodi@gmail.com>
2024-05-08 05:31:44 +0530

    Update RestAPI.ps1

--- Rest_API/RestAPI.ps1
+++ Rest_API/RestAPI.ps1
@@ -1,1 +1,49 @@
+param (
+    [string]$Username,
+    [string]$Password
+)
 
+# Initialize JSON result object
+$jsonResult = @{
+    prtg = @{
+        result = @()
+        error  = $null
+    }
+}
+
+# Check if username and password are provided
+if (-not $Username -or -not $Password) {
+    $jsonResult.prtg.error = 1
+    $jsonResult.prtg.text = "Username and password are required."
+    Write-Output ($jsonResult | ConvertTo-Json -Depth 100)
+    exit 1
+}
+
+# Define the API URL
+$apiUrl = "ENTER_API_URL_HERE"
+
+# Create a credential object
+$credential = New-Object System.Management.Automation.PSCredential ($Username, (ConvertTo-SecureString $Password -AsPlainText -Force))
+
+try {
+    # Make the HTTP request with authentication
+    $response = Invoke-RestMethod -Uri $apiUrl -Method Get -Credential $credential
+   
+    # Extract the specific data from the response
+    $data = $response.receive.data
+
+    # Add balance to JSON result
+    $jsonResult.prtg.result += @{
+        channel = "Name"
+        value = [INT]$data
+        unit = "Custom"  
+    }
+
+} catch {
+    $jsonResult.prtg.error = 1
+    $jsonResult.prtg.text = "Failed to retrieve data: $($_.Exception.Message)"
+}
+
+# Output the JSON result
+$jsonOutput = $jsonResult | ConvertTo-Json -Depth 100
+Write-Output $jsonOutput
